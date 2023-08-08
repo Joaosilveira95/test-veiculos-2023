0ff9e5fcb2da11e70d9218992e3689790059260a
João Vitor Silveira <jv.silvasilveira95@gmail.com>
2023-08-08 14:45:35 -0300

    Finalizando Test.

--- backend/src/main/java/com/example/backend/entity/Veiculo.java
+++ backend/src/main/java/com/example/backend/entity/Veiculo.java
@@ -13,84 +13,99 @@ import jakarta.persistence.TemporalType;
 @Entity
 @Table(name = "veiculo")
 public class Veiculo {
-	
-	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
 
-    private String veiculo;
-    
-    private String marca;
-    
-    private Integer ano;
-    
-    private String descricao;
-    
-    private boolean vendido;
-    
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date dataCriacao;
-    
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date dataAtualizacao;
-    
-    private String chassi;
-    
-    private Double preco;
-    
-    
-    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String veiculo;
+
+	private String marca;
+
+	private Integer ano;
+
+	private String descricao;
+
+	private boolean vendido;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataCriacao;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataAtualizacao;
+
+	private String chassi;
+
+	private Double preco;
+
 	public String getVeiculo() {
 		return veiculo;
 	}
+
 	public void setVeiculo(String veiculo) {
 		this.veiculo = veiculo;
 	}
+
 	public String getMarca() {
 		return marca;
 	}
+
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
+
 	public Integer getAno() {
 		return ano;
 	}
+
 	public void setAno(Integer ano) {
 		this.ano = ano;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public boolean isVendido() {
 		return vendido;
 	}
+
 	public void setVendido(boolean vendido) {
 		this.vendido = vendido;
 	}
+
 	public String getChassi() {
 		return chassi;
 	}
+
 	public void setChassi(String chassi) {
 		this.chassi = chassi;
 	}
+
 	public Double getPreco() {
 		return preco;
 	}
+
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
+
 	public Date getDataCriacao() {
 		return dataCriacao;
 	}
+
 	public void setDataCriacao(Date dataCriacao) {
 		this.dataCriacao = dataCriacao;
 	}
+
 	public Date getDataAtualizacao() {
 		return dataAtualizacao;
 	}
+
 	public void setDataAtualizacao(Date dataAtualizacao) {
 		this.dataAtualizacao = dataAtualizacao;
 	}
